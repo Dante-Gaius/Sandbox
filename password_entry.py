f9d3b174e90036ea07df0e52f1992dac4080c415
Dante-Gaius <dante.gaius@my.jcu.edu.au>
2018-08-15 19:06:36 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,10 @@
 """Dante Gaius"""
+
+password = input("Enter password: ")
+
+while len(password) < 6:
+    print("Invalid password")
+    password = input("Enter password: ")
+
+for char in password:
+    print("*", end="")
